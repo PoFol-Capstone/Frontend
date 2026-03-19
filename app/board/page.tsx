8e8f80d3cade804be6aefb53c7f22cdef569ff72
HwangHotae <hotae0321@naver.com>
2026-03-19 11:49:04 +0900

    feat : 게시판 페이지

---
+++ app/board/page.tsx
@@ -0,0 +1,7 @@
+export default function BoardPage() {
+  return (
+    <div className="flex flex-1 flex-col">
+      <p>게시판 피드 페이지</p>
+    </div>
+  );
+}
---
+++ app/board/write/page.tsx
@@ -0,0 +1,7 @@
+export default function BoardWritePage() {
+  return (
+    <div className="flex flex-1 flex-col">
+      <p>게시판 작성 페이지</p>
+    </div>
+  );
+}
