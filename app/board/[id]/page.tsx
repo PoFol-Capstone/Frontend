23fccd328fc0d1f5ca7f5265aadb6a4f1fc44be4
surimchai <slchai0903@gmail.com>
2026-05-27 18:05:10 +0900

    fix: 중복 import 제거

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -3,7 +3,6 @@ import { getPost, getRelatedPosts } from "@/lib/post";
 import { getSessionUuid } from "@/lib/session";
 import getUser from "@/lib/user";
 import PostDetail from "./_components/PostDetail/index";
-import getUser from "@/lib/user";
 
 export default async function PostDetailPage({
   params,
